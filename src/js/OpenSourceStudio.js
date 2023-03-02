04199eb272aaa11195ab6059de496c036b5609a6
Patryk Opala <patrykopala265@gamil.com>
2023-03-02 09:34:53 +0100

    0.0.2

--- src/js/OpenSourceStudio.js
+++ src/js/OpenSourceStudio.js
@@ -4,12 +4,17 @@ import {Size} from './Component.js';
 class OpenSourceStudio{
   constructor(rootElement){
     this.root = rootElement;
+    this.editorMode = "Normal";
     //this.advancedMenu = new AdvancedMenu(this.root, new Size(this.root.offsetWidth, 30));
   }
 
   init(){
     const advancedMenu = new AdvancedMenu(this.root, new Size(this.root.offsetWidth, 30));
     advancedMenu.initMenu();
+  }
+
+  changeEditorMode(mode){
+    this.editorMode = mode;
   }
 }
 
